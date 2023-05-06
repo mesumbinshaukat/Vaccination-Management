1dd08f4e81422932398fd00ea3f182f20041afd7
Zohair Adeel <zohairadeel987@gmail.com>
2023-05-06 14:44:30 +0500

    update

--- allhospital.php
+++ allhospital.php
@@ -1,8 +1,7 @@
 <?php
 session_start();
 include('connection.php');
-
-$squery= "SELECT * FROM `addvaccine`";
+$squery= "SELECT * FROM `hospital_register`";
 $q_run = mysqli_query($conn , $squery);
 ?>
 
@@ -59,22 +58,22 @@ $q_run = mysqli_query($conn , $squery);
 <body class="">
 
 <?php include('navbar.php')  ?>
-
-
-<div class="card-group">
 <?php while($row = mysqli_fetch_array($q_run)) { ?>
 
+<div class="container col-lg4">
+<div class="card-group">
   <div class="card">
-    <img src="<?php echo $row[''] ?>" class="card-img-top" alt="...">
+    <img src="<?php echo $row['Hlogo'] ?>" class="card-img-top w-25" alt="...">
     <div class="card-body">
-      <h5 class="card-title">Card title</h5>
+      <h5 class="card-title"><?php echo $row['Hname'] ?></h5>
       <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
       <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
     </div>
   </div>
-
-<?php }?>
 </div>
+</div>
+<?php }?>
+
 
 
 
